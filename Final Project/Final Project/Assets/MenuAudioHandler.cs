4a2fcf3b5d3d28495c5416c05f99a654f0034fc2
Austin Moore <austin19moore@gmail.com>
2024-05-05 00:53:23 -0500

    Fixed

--- Final Project/Final Project/Assets/MenuAudioHandler.cs
+++ Final Project/Final Project/Assets/MenuAudioHandler.cs
@@ -1,31 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MenuAudioHandler : MonoBehaviour
 {
-    private static MenuAudioHandler singleton = null;
+    public static MenuAudioHandler singleton = null;
     private AudioSource sound;
 
     void Start() {
         sound = GetComponent<AudioSource>();
     }
 
-    public static MenuAudioHandler Instance {
-        get {
-            return singleton;
-        }
-    }
-
     void Awake() {
-        if (singleton != this && singleton != null) {
+        if (singleton != null) {
             Destroy(this.gameObject);
             return;
         } else {
             singleton = this;
+            DontDestroyOnLoad(this.gameObject);
         }
-
-        DontDestroyOnLoad(this.gameObject);
 
     }
 
